1798200cd9fa0991db786bb84c216ee735b34fd1
Nikolina Krunic <krunicnina@gmail.com>
2022-11-08 01:06:06 +0100

    inicijalizovan kostur

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kozmetički salon Afrodita</title>
+    <link rel="stylesheet" href="css/dizajn.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><img src="slike/pp.png"></label>
+        <ul>
+            <li><a class="active" href="index.php">Početna</a></li>
+            <li><a href="#">Zakazivanje termina</a></li>
+            <li><a href="tretmani.php">Estetski tretmani</a></li>
+            <li><a href="contact.html">Kontakt</a></li>
+        </ul>
+    </nav>
+    
+
+<div class="baner">
+  <h1><b>Kozmetički salon Afrodita</b></h1>
+  <!-- <p id="DatumVreme"></p> -->
+ <br>
+   <a href="forma.php">Prijavi se</a> 
+ 
+  <br><br>
+</div>
+<div class="wrapper">
+    <h1>Aktuelne vesti</h1>
+    <div class="img-area">
+      <div class="single-img"><img src="slike/lice.jpg" alt="">
+        <h2>Akcijske cene botoksa u decembru!</h2>
+        <p>Iskoristite sjajne popuste koje Vam Afrodita <br>nudi i Vaša koža će Vam biti večno zahvalna!</p> 
+      </div>
+      <div class="single-img"><img src="slike/epilacija.jpg">
+        <h2>Laserska epilacija</h2>
+        <p> Svaki peti tretman laserske epilacije<br> regije po izboru je besplatan. </p> 
+      </div>
+      <div class="single-img"><img src="slike/novagodina.jpg">
+        <h2>Neradni dani</h2>
+        <p>U periodu od 31.12.2022. do 15.1.2023. salon neće raditi <br> zbog praznika.</p> 
+      </div>
+    </div>
+  </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ tretmani.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estetski tretmani</title>
+    <link rel="stylesheet" href="css/tretmani1.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><img src="slike/pp.png"></label>
+        <ul>
+            <li><a class="active" href="index.php">Početna</a></li>
+            <li><a href="#">Zakazivanje termina</a></li>
+            <li><a href="tretmani.php">Estetski tretmani</a></li>
+            <li><a href="contact.html">Kontakt</a></li>
+        </ul>
+    </nav>
+    <div class="baner">
+    </div>
+    <div class="wrapper">
+        <h1>Estetski tretmani u našem salonu</h1>
+        <div class="img-area">
+          <div class="single-img"><img src="slike/lashlift.jpg" alt="">
+            <h2>Lash lift</h2>
+            <p>Tretman donosi uvijanje, bojenje i keratinsko <br>jačanje trepavica i u nekoliko koraka <br> dobijate idealno oblikovane, <br> guste, tamne, 
+            duge i jednom rečju - savršene trepavice.
+            </p> 
+          </div>
+          <div class="single-img"><img src="slike/browlift.jpg" alt="">
+            <h2>Brow lift</h2>
+            <p>Brow lift je tretman dugotrajne fiksacije obrva. <br> Prvo se nanosi preparat koji razmekša dlaku, <br> zatim se obrve boje (ukoliko klijent želi)
+             <br> i na kraju se fiksiraju.</p> 
+          </div>
+          <div class="single-img"><img src="slike/microblading.jpg" alt="">
+            <h2>Microblading</h2>
+            <p>Microblading je tehnika iscrtavanja obrva zahvaljujući <br> kojoj one deluju pre svega prirodno. <br>Taj efekat se postiže specijalnim nožicem koji<br> imitira oblik i položaj prirodne dlake.
+            </p> 
+          </div>
+          <div class="single-img"><img src="slike/svilene.jpg" alt="">
+            <h2>Svilene trepavice</h2>
+            <p> Potpuno prirodne, neškodljive, lagane ekstenzije <br>  od svile, koje izuzetno prijanjaju na <br> Vaše prirodne trepavice i daju im duži i gušći izgled. 
+            <br> Ova potpuno visokokvalitetna metoda imitacije <br> prirodnih trepavica učiniće Vaš pogled neodoljivim.</p> 
+          </div>
+          <div class="single-img"><img src="slike/botox.jpg" alt="">
+            <h2>Botox tretman</h2>
+            <p> Botox tretman je najbolja prevencija i  <br> korekcija mimičnih bora kao znaka starenja <br> 
+            Izvodi se tako da se preparat aplikuje <br> pod kožu  na mestima gde se bore <br> žele ukloniti ili umanjiti. <br></p> 
+          </div>
+          <div class="single-img"><img src="slike/fileri.jpg" alt="">
+            <h2>Hijaluronski fileri</h2>
+            
+            <p>Hijaluronskim filerima se popunjavaju jagodice, <br> nazolabijalne brazde, pušačke bore,<br> usne, donja vilica i podočnjaci. <br>  Tretman je bezbolan, traje kratko,  <br> a efekti su vidljivi momentalno. 
+              </p> 
+          </div>
+        </div>
+      </div>
+</body>
+</html>
